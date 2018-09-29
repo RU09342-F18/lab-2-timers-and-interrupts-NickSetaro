40dc330dd9cb7020b72cfa32cc2a2d7dce182b7b
NickSetaro <33162219+NickSetaro@users.noreply.github.com>
2018-09-29 13:06:26 -0400

    edited msp432

--- Button Interrupt/Button Interrupt MSP432P401R/main.c
+++ Button Interrupt/Button Interrupt MSP432P401R/main.c
@@ -1,14 +1,75 @@
 #include <msp432.h>
 
-/*
- * @author Nick Setaro
- * @version 9.28.2018
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2013, Texas Instruments Incorporated
+ * All rights reserved.
  *
- * This code uses the msp432P401R microcontroller to toggle an LED when a button is pressed.
- * A button interrupt is used to wake the cpu to toggle the LED.
- * Debouncing is included to keep the cpu from registering multiple incorrect interrupts.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- */
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP432 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP432 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see http://www.ti.com/tool/mspdriverlib for an API functional
+ * library & https://dev.ti.com/pinmux/ for a GUI approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//  MSP432P401 Demo - Software Port Interrupt Service on P1.1 from LPM4
+//
+//  Description: MSP432 device is configured to enter LPM4 mode with GPIOs
+//  properly terminated. P1.1 is configured as an input. Pressing the button
+//  connect to P1.1 results in device waking up and servicing the Port1 ISR.
+//  LPM3 current can be measured when P1.0 is output low (e.g. LED off).
+//
+//  ACLK = 32kHz, MCLK = SMCLK = default DCO
+//
+//
+//               MSP432P401x
+//            -----------------
+//        /|\|                 |
+//         | |                 |
+//         --|RST              |
+//     /|\   |                 |
+//      --o--|P1.1         P1.0|-->LED
+//     \|/
+//
+//   Dung Dang
+//   Texas Instruments Inc.
+//   Oct 2016 (updated) | November 2013 (created)
+//   Built with CCSv6.1, IAR, Keil, GCC
+//******************************************************************************
+
 int main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;     //STOP WATCHDOG TIMER
